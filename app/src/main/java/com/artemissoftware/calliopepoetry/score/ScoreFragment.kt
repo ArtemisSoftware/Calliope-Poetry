cf39816d5001cfca1fef0ccce5cb2e3fb79bac29
gustavo <gustavo.maia@b2b.com.pt>
2020-12-11 11:42:34 +0000

    ScoreFragment

--- app/src/main/java/com/artemissoftware/calliopepoetry/score/ScoreFragment.kt
+++ app/src/main/java/com/artemissoftware/calliopepoetry/score/ScoreFragment.kt
@@ -2,8 +2,12 @@ package com.artemissoftware.calliopepoetry.score
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.artemissoftware.calliopepoetry.R
 import com.artemissoftware.calliopepoetry.databinding.ScoreFragmentBinding
 import com.artemissoftware.calliopepoetry.game.GameViewModel
@@ -13,7 +17,9 @@ class ScoreFragment : Fragment(R.layout.score_fragment){
     private var _binding : ScoreFragmentBinding? = null
     private val binding get() = _binding!!
 
-    //private lateinit var viewModel: GameViewModel
+    private val args by navArgs<ScoreFragmentArgs>()
+
+    private lateinit var viewModel: ScoreViewModel
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -21,7 +27,23 @@ class ScoreFragment : Fragment(R.layout.score_fragment){
 
         _binding = ScoreFragmentBinding.bind(view)
 
-        //viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(ScoreViewModel::class.java)
+
+        binding.apply {
+
+            scoreViewModel = viewModel
+            lifecycleOwner  = viewLifecycleOwner
+            score = args.score
+        }
+
+        // Navigates back to game when button is pressed
+
+        viewModel.eventPlayAgain.observe(viewLifecycleOwner, Observer { playAgain ->
+            if (playAgain) {
+                findNavController().navigate(ScoreFragmentDirections.actionScoreFragmentToGameFragment())
+                viewModel.onPlayAgainComplete()
+            }
+        })
 
     }
 
